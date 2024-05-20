21fc67db175a925ce1a32d303a53b44ed01aed73
sanelp2 <20240709@itla.edu.do>
2024-05-20 13:48:54 -0400

    primeros 4 ejercicios(sin comentarios)

---
+++ ejerccap02/ejer01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(void)
+{
+    float PRO;
+    printf("Ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+        printf("\nAprobado");
+}
---
+++ ejerccap02/ejer02/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+    float PRO, VPR;
+    printf("Ingrese el precio del producto: ");
+    scanf("%f", &PRE);
+    if (PRO < 1500)
+    {
+        NPR = PRE * 1.11;
+        prntf("\nNuevo precio: %7.2f" ,NPR);
+    }
+}
